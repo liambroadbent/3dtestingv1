36f221af8f7f105c3e820caf3659fc86cf9879a5
Liam <lbroadbent21+github@gmail.com>
2022-01-05 22:38:55 +0000

    sorting the text-box3

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ function init() {
   //Create scene
   scene = new THREE.Scene();
 
-  const fov = 45;
+  const fov = 35;
   const aspect = container.clientWidth / container.clientHeight;
   const near = 0.1;
   const far = 1000;
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 5, 40);
+  camera.position.set(0, 5, 50);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
